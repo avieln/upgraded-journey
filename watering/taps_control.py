6513fdb449f8a09137999c7b6304e13ed3312489
avielnah <aviel.nah@gmail.com>
2018-06-15 15:56:59 +0300

    first commit

---
+++ watering/taps_control.py
@@ -0,0 +1,15 @@
+
+
+class TapsControl:
+
+    def __init__(self):
+        pass
+
+    def get_channels(self):
+        pass
+    @staticmethod
+    def open_channel(channel_num):
+        print('Opening channel ' + str(channel_num))
+    @staticmethod
+    def close_channel(self, channel_num):
+        print('Closing channel ' + str(channel_num))
---
+++ watering/water_sched.py
@@ -0,0 +1,54 @@
+import taps_control
+from schedule import Scheduler
+import threading
+import time
+import json
+
+
+def job_func(channel, duration):
+
+    def f():
+        print(thread.name + ' started channel ' + str(channel))
+        taps_control.TapsControl.open_channel(channel_num=channel)
+        time.sleep(duration)
+        print( thread.name +' finished channel ' + str(channel))
+        taps_control.TapsControl.open_channel(channel_num=channel)
+
+    thread = threading.Thread(target=f)
+    thread.start()
+    return
+
+
+class Server:
+
+    def __init__(self):
+        self.scheduler = Scheduler()
+        self.semaphore = threading.Semaphore()
+        server.read_config()
+
+    def read_config(self):
+        with open('config.json', 'r') as f:
+            data = json.load(f)
+            for job in data.values():
+                self.handle_job(job)
+
+    def handle_job(self, job):
+
+        when = job['when']
+        start = job['start']
+
+        if when == 'daily':
+            self.scheduler.every().day.at(start).do(job_func, int(job['channel']), int(job['duration']))
+
+        else:
+            getattr(self.scheduler.every(), when).at(start).do(job_func, int(job['channel']), int(job['duration']))
+
+    def run(self):
+
+        while True:
+            self.scheduler.run_pending()
+
+
+server = Server()
+
+server.run()
