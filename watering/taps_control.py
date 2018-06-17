9d36383fa9915ba6f96f45bb3df463d88ecefe3c
Aviel <aviel.nah@gmail.com>
2018-06-18 00:05:15 +0300

    d

--- watering/taps_control.py
+++ watering/taps_control.py
@@ -1,20 +1,27 @@
-import RPi.GPIO as GPIO
+from RPi import GPIO as GPIO
 
 class TapsControl:
 
-    def __init__(self):
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(7, GPIO.OUT)
+    	def __init__(self):
+        	GPIO.setmode(GPIO.BOARD)
+	        GPIO.setup(7, GPIO.OUT)
+		GPIO.setup(11,GPIO.OUT)
+		GPIO.setup(13,GPIO.OUT)
 
-    def get_channels(self):
-        pass
+	def get_channels(self):
+        	pass
 
-    @staticmethod
-    def open_channel(channel_num):
-        GPIO.ouput(channel_num, 1)
-        print('Opening channel ' + str(channel_num))
+	@staticmethod
+	def open_channel(channel_num):
+        	GPIO.output(channel_num, 1)
+	        print('Opening channel ' + str(channel_num))
 
-    @staticmethod
-    def close_channel(self, channel_num):
-        GPIO.ouput(channel_num, 0)
-        print('Closing channel ' + str(channel_num))
+	@staticmethod
+	def close_channel(channel_num):
+        	GPIO.output(channel_num, 0)
+        	print('Closing channel ' + str(channel_num))
+
+	def  __del__(self):
+		GPIO.cleanup()
+
+
--- watering/water_sched.py
+++ watering/water_sched.py
@@ -3,16 +3,16 @@ from schedule import Scheduler
 import threading
 import time
 import json
+import os
 
-
-def job_func(channel, duration):
+def job_func(channel, duration,controller):
 
     def f():
         print(thread.name + ' started channel ' + str(channel))
-        taps_control.TapsControl.open_channel(channel_num=channel)
+        controller.open_channel(channel_num=channel)
         time.sleep(duration)
         print( thread.name +' finished channel ' + str(channel))
-        taps_control.TapsControl.open_channel(channel_num=channel)
+        controller.close_channel(channel_num=channel)
 
     thread = threading.Thread(target=f)
     thread.start()
@@ -24,22 +24,25 @@ class Server:
     def __init__(self):
         self.scheduler = Scheduler()
         self.semaphore = threading.Semaphore()
-        server.read_config()
+	self.con = taps_control.TapsControl()
+	self.read_config()
 
     def read_config(self):
-        with open('config.json', 'r') as f:
+	config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'config.json') 
+        with open(config_path, 'r') as f:
             data = json.load(f)
+	    print(data)
             for job in data.values():
                 self.handle_job(job)
 
     def handle_job(self, job):
-
+	
         when = job['when']
         start = job['start']
-
+	con = self.con
         if when == 'daily':
-            self.scheduler.every().day.at(start).do(job_func, int(job['channel']), int(job['duration']))
-            self.scheduler.every().minute.do(job_func, int(job['channel']), 0.5)
+	    print('Queued a daily job on channel '+ str(job['channel']))
+            self.scheduler.every(15).seconds.do(job_func, int(job['channel']), 0.01, con)
         else:
             getattr(self.scheduler.every(), when).at(start).do(job_func, int(job['channel']), int(job['duration']))
 
