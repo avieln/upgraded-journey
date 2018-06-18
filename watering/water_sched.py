eb9c2127e4595a381ac55b7a65dcf72255e11818
avielnah <aviel.nah@gmail.com>
2018-06-18 08:13:52 +0300

    fix indent

--- watering/water_sched.py
+++ watering/water_sched.py
@@ -24,24 +24,23 @@ class Server:
     def __init__(self):
         self.scheduler = Scheduler()
         self.semaphore = threading.Semaphore()
-	self.con = taps_control.TapsControl()
-	self.read_config()
+        self.con = taps_control.TapsControl()
+        self.read_config()
 
     def read_config(self):
-	config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'config.json') 
+        config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'config.json')
         with open(config_path, 'r') as f:
             data = json.load(f)
-	    print(data)
+            print(data)
             for job in data.values():
                 self.handle_job(job)
 
     def handle_job(self, job):
-	
         when = job['when']
         start = job['start']
-	con = self.con
+        con = self.con
         if when == 'daily':
-	    print('Queued a daily job on channel '+ str(job['channel']))
+            print('Queued a daily job on channel '+ str(job['channel']))
             self.scheduler.every(15).seconds.do(job_func, int(job['channel']), 0.01, con)
         else:
             getattr(self.scheduler.every(), when).at(start).do(job_func, int(job['channel']), int(job['duration']))
